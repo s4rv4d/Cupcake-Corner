00a586eef90cc57a994e5a93044de694271fc3be
s4rv4d <sarvadshetty@gmail.com>
2020-01-21 19:40:12 +0530

    manual conformance to codable for property wrappers

--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -8,6 +8,29 @@
 
 import SwiftUI
 
+class User: ObservableObject, Codable {
+    
+    //creating codingkeys enum
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    //properties
+    @Published var name = "Sarvad"
+    
+    //init
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+         name = try container.decode(String.self, forKey: .name)
+    }
+    
+    //function
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
