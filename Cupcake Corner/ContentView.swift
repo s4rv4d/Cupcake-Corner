363d545bd80768478e6bfe02038c66aea7d8a9e5
s4rv4d <sarvadshetty@gmail.com>
2020-01-22 07:09:46 +0530

    URLSession with codable

--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -8,32 +8,51 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    
-    //creating codingkeys enum
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    //properties
-    @Published var name = "Sarvad"
-    
-    //init
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-         name = try container.decode(String.self, forKey: .name)
-    }
-    
-    //function
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
 
 struct ContentView: View {
+    
+    @State private var results = [Result]()
+    
     var body: some View {
-        Text("Hello, World!")
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName)
+            }
+        }
+    .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            fatalError("url not valid")
+        }
+        
+        let request = URLRequest(url: url)
+        
+        //create session
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = decodedResponse.results
+                    }
+                    return
+                }
+            }
+            print("failed to load: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
     }
 }
 
