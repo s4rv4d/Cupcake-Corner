572aec1604605c8d146ee936273c1204aa471c9c
s4rv4d <sarvadshetty@gmail.com>
2020-01-22 07:16:32 +0530

    trying out .disabled()

--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -8,51 +8,29 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
     
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
     
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName)
-            }
-        }
-    .onAppear(perform: loadData)
+    var disabledForm: Bool {
+        username.count < 5 || email.count < 5
     }
-    
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            fatalError("url not valid")
-        }
         
-        let request = URLRequest(url: url)
-        
-        //create session
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    DispatchQueue.main.async {
-                        self.results = decodedResponse.results
-                    }
-                    return
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("creating account ...")
                 }
             }
-            print("failed to load: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
+            .disabled(disabledForm)
+        }
     }
 }
 
